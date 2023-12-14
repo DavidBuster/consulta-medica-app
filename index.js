071d34dd4adbab27639d69c532e67831e2881ccb
David Buster <david.buster@saurus.com>
2023-12-14 13:29:02 +0100

    index and index2 swapped

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-const http = require("http");
+const express = require("express");
+const morgan = require("morgan");
+// const cors = require("cors");
+
+const app = express();
+
+// app.use(cors());
+
+app.use(express.json());
+
+// MORGAN LOGGER
+app.use(morgan("tiny"));
+
+// MANUAL LOGGER
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(requestLogger);
 
 let notes = [
   {
@@ -17,11 +39,68 @@ let notes = [
     important: true,
   },
 ];
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(notes));
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
 });
 
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  console.log("PRE", notes);
+  notes = notes.filter((note) => note.id !== id);
+
+  console.log("POST", notes);
+
+  response.status(204).end();
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on portAAA ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
