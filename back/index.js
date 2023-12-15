e0b3cc392c06a1deab9fab7db597474caf7f5730
David Buster <david.buster@saurus.com>
2023-12-15 11:23:40 +0100

    Reestructure projects and add dist folder to back

--- back/index.js
+++ back/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const morgan = require("morgan");
-// const cors = require("cors");
+// const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
-// app.use(cors());
+app.use(express.static("dist"));
+
+app.use(cors());
 
 app.use(express.json());
 
 // MORGAN LOGGER
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
 
 // MANUAL LOGGER
 const requestLogger = (request, response, next) => {
